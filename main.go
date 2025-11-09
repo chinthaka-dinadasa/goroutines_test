a5baba922fdfc08a096d7e035bd0ad0ae6728320
chinthaka-dinadasa <sendtoclabz@gmail.com>
2025-11-09 11:05:54 +0530

    added worker groups

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
-func captureWeatherInfo(city string) interface{} {
+func captureWeatherInfo(city string, ch chan<- string, wg *sync.WaitGroup) interface{} {
 	var response struct {
 		Data struct {
 			Temperature float32 `json:"temperature"`
 			Country     string  `json:"country"`
 		} `json:"data"`
 	}
+
+	defer wg.Done()
 
 	apiUrl := fmt.Sprintf("http://localhost:3000/weather/%s", city)
 	resp, err := http.Get(apiUrl)
@@ -26,17 +29,31 @@ func captureWeatherInfo(city string) interface{} {
 		fmt.Printf("Error decoding incoming weather info %v \n", err)
 		return response
 	}
+	ch <- fmt.Sprintf("This is the city %s", city)
 	return response
 }
 
 func main() {
 	startNow := time.Now()
 	fmt.Println("Hello API")
+
+	ch := make(chan string)
+	var wg sync.WaitGroup
+
 	cities := []string{"London", "New York", "Tokyo", "Paris", "Sydney"}
 
 	for _, city := range cities {
-		data := captureWeatherInfo(city)
-		fmt.Printf("City weather data %v \n", data)
+		wg.Add(1)
+		go captureWeatherInfo(city, ch, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for result := range ch {
+		fmt.Println(result)
 	}
 
 	fmt.Printf("Took %v to complete capture weather data from API", time.Since(startNow))
